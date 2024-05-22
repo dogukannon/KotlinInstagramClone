4e0831c6cd6cd5efb91f6a3b3138a99e96ae6930
dogukan <dogukanonmaz@gmail.com>
2024-05-23 01:06:31 +0300

    Initial commit

---
+++ app/src/main/java/com/dogukan/kotlininstagramclone/FeedActivity.kt
@@ -0,0 +1,51 @@
+package com.dogukan.kotlininstagramclone
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dogukan.kotlininstagramclone.databinding.ActivityFeedBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class FeedActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityFeedBinding
+    private lateinit var auth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =ActivityFeedBinding.inflate(layoutInflater)
+        val view =binding.root
+        setContentView(view)
+
+        auth = Firebase.auth
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater= menuInflater
+        menuInflater.inflate(R.menu.insta_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.add_post){
+            val intent = Intent(this,UploadActivity::class.java)
+            startActivity(intent)
+        }else if (item.itemId == R.id.sign_out){
+            auth.signOut()
+            val intent =Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+        return super.onOptionsItemSelected(item)
+    }
+}
